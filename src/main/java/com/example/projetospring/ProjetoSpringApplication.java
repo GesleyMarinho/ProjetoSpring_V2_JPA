013d62349f0cfe21eeb06a1efae7a1bf24aad678
GesleyMarinho <gesleymarinho@gmail.com>
2026-01-25 10:22:31 -0300

    teste

--- src/main/java/com/example/projetospring/ProjetoSpringApplication.java
+++ src/main/java/com/example/projetospring/ProjetoSpringApplication.java
@@ -9,6 +9,6 @@ public class ProjetoSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjetoSpringApplication.class, args);
 
-        System.out.println("Hello World !");
+        System.out.println("Hello World");
     }
 }
