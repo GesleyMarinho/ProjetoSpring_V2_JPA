5e78900f4798ecf06427f5fc4ed9bf0e1c88aec2
GesleyMarinho <gesleymarinho@gmail.com>
2026-02-23 18:50:15 -0300

    Ajuste no user services, e finalização do projeto básico de crud

--- src/main/java/com/example/projetospring/services_funcoes/UserService.java
+++ src/main/java/com/example/projetospring/services_funcoes/UserService.java
@@ -6,7 +6,6 @@ import com.example.projetospring.services_funcoes.exceptions.ResourceNotFoundExc
 import com.example.projetospring.repositories_acesso_banco.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
